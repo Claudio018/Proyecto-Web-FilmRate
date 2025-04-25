3750cea10b80c2ea99ee477952e87cd1f57f70ae
Claudio018 <claudio.cabello.v@mail.pucv.cl>
2025-04-25 10:32:21 -0400

    Creación carpeta models con interface peliculas y usuarios

---
+++ src/app/models/peliculas.ts
@@ -0,0 +1,10 @@
+export interface Peliculas {
+    id:number,
+    titulo:string,
+    año: string,
+    descripcion:string,
+    director:string,
+    genero:string,
+    imagen:string,
+    rating:number
+}
