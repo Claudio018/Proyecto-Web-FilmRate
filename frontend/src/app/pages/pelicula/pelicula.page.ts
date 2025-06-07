0b72414c2645ab87e2aac42f8bd88268c787ce93
Dmarcan <david.martinez.c@mail.pucv.cl>
2025-06-06 23:18:34 -0400

    correcion de banner

--- frontend/src/app/pages/pelicula/pelicula.page.ts
+++ frontend/src/app/pages/pelicula/pelicula.page.ts
@@ -15,6 +15,8 @@ export class PeliculaPage implements OnInit {
   trailerUrl: string | null = null;
   resenas: any[] = [];
 
+  bannerExpandido = false;
+
   constructor(
     private route: ActivatedRoute,
     private tmdbService: TmbdService,
@@ -27,37 +29,34 @@ export class PeliculaPage implements OnInit {
   }
 
   loadMovie() {
-    // Detalle de la película
     this.tmdbService.getMovieDetail(this.peliculaId).subscribe(data => {
       this.pelicula = data;
     });
 
-    // Obtener videos
     this.tmdbService.getMovieVideos(this.peliculaId).subscribe(data => {
       const videos = data.results;
-
       let trailer = videos.find((video: any) =>
         video.type === 'Trailer' && video.site === 'YouTube'
       );
 
       if (trailer) {
         this.trailerUrl = `https://www.youtube.com/embed/${trailer.key}`;
-        console.log('Se cargó el tráiler desde YouTube (tipo: Trailer):', trailer.name);
       } else {
         trailer = videos.find((video: any) => video.site === 'YouTube');
         if (trailer) {
           this.trailerUrl = `https://www.youtube.com/embed/${trailer.key}`;
-          console.log('No se encontró un tráiler, pero se cargó otro video de YouTube:', trailer.name);
         } else {
           this.trailerUrl = null;
-          console.warn('No se encontró ningún video disponible para esta película.');
         }
       }
     });
 
-    // Obtener reseñas
     this.resenaService.getResenasByPelicula(this.peliculaId).subscribe(data => {
       this.resenas = data;
     });
+  }
+
+  toggleBanner() {
+    this.bannerExpandido = !this.bannerExpandido;
   }
 }
