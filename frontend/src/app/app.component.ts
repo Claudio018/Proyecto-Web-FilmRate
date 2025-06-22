1a259fab478e8f840a2c44b8fb9f77fb2b71ee25
Dmarcan <david.martinez.c@mail.pucv.cl>
2025-06-22 15:41:10 -0400

    correccion de error modo oscuro y modo claro

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -8,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   constructor() {}
+  ngOnInit() {
+    const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+    // Aplicar el modo actual al cargar
+    this.updateDarkMode(darkModeMediaQuery.matches);
+
+    // Escuchar cambios en tiempo real
+    darkModeMediaQuery.addEventListener('change', (event) => {
+      this.updateDarkMode(event.matches);
+    });
+  }
+
+  updateDarkMode(isDarkMode: boolean) {
+    if (isDarkMode) {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+    }
+  }
+
+
 }
